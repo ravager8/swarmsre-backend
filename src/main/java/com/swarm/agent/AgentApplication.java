cf14882c90f84c98ce269a6d2a079b2fc1e8eea7
Sugam Sharma <himanshu@Sugams-MacBook-Air.local>
2026-05-17 00:36:34 +0530

    Initial commit : Spring Boot SwarmSRE skeleton

---
+++ src/main/java/com/swarm/agent/AgentApplication.java
@@ -0,0 +1,14 @@
+package com.swarm.agent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgentApplication {
+
+	public static void main(String[] args) {
+        System.out.println("Agents ready for the kill");
+		SpringApplication.run(AgentApplication.class, args);
+	}
+
+}
